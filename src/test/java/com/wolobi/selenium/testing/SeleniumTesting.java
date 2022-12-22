63d8b64612c127592314ccd14e9dc0e782c9ed47
Renan <renan.specicaste@gmail.com>
2022-12-22 12:33:31 +0100

    fix: relative path to selenium chrome driver

--- src/test/java/com/wolobi/selenium/testing/SeleniumTesting.java
+++ src/test/java/com/wolobi/selenium/testing/SeleniumTesting.java
@@ -23,7 +23,7 @@ public class SeleniumTesting {
 //    }
     @Test
     public void jenkinsTest() {
-        System.setProperty("webdriver.chrome.driver", "G:\\cursos\\jenkins\\selenium-drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "selenium-drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.amazon.es/");
